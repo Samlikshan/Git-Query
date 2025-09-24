f55ac86d6fec2df38d9cf33a908ff3615d7a50b8
samlik shan <samlikshanm@gmail.com>
2025-09-24 19:38:32 +0530

    Add server configuration for cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import { corsConfig } from "./config/config";
 import { errorMiddleware } from "./presentation/middlewares/erorr.middleware";
 import indexrouter from "./presentation/routes/index";
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(corsConfig));
 
 app.use("/api", indexrouter);
 
